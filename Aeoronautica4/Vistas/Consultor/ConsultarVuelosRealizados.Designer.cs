078bbf1bdf007bd8361d3a5b036d1d9eb02ef85e
Mat1cL <matitorres28@gmail.com>
2016-11-04 00:49:45 -0300

    adding project

--- Aeoronautica4/Vistas/Consultor/ConsultarVuelosRealizados.Designer.cs
+++ Aeoronautica4/Vistas/Consultor/ConsultarVuelosRealizados.Designer.cs
@@ -31,20 +31,26 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConsultarVuelosRealizados));
             this.dtgVuelos = new System.Windows.Forms.DataGridView();
             this.btnVuelos = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtDesde = new System.Windows.Forms.DateTimePicker();
+            this.dtHasta = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnFiltrar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dtgVuelos)).BeginInit();
             this.SuspendLayout();
             // 
             // dtgVuelos
             // 
             this.dtgVuelos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgVuelos.Location = new System.Drawing.Point(12, 31);
+            this.dtgVuelos.Location = new System.Drawing.Point(12, 164);
             this.dtgVuelos.Name = "dtgVuelos";
             this.dtgVuelos.Size = new System.Drawing.Size(1119, 287);
             this.dtgVuelos.TabIndex = 0;
             // 
             // btnVuelos
             // 
-            this.btnVuelos.Location = new System.Drawing.Point(506, 345);
+            this.btnVuelos.Location = new System.Drawing.Point(506, 478);
             this.btnVuelos.Name = "btnVuelos";
             this.btnVuelos.Size = new System.Drawing.Size(117, 41);
             this.btnVuelos.TabIndex = 1;
@@ -52,18 +58,85 @@
             this.btnVuelos.UseVisualStyleBackColor = true;
             this.btnVuelos.Click += new System.EventHandler(this.btnVuelos_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(441, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "VUELOS REALIZADOS";
+            // 
+            // dtDesde
+            // 
+            this.dtDesde.CustomFormat = "dd/MM/yyyy";
+            this.dtDesde.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtDesde.Location = new System.Drawing.Point(446, 103);
+            this.dtDesde.Name = "dtDesde";
+            this.dtDesde.Size = new System.Drawing.Size(89, 20);
+            this.dtDesde.TabIndex = 15;
+            // 
+            // dtHasta
+            // 
+            this.dtHasta.CustomFormat = "dd/MM/yyyy";
+            this.dtHasta.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtHasta.Location = new System.Drawing.Point(585, 103);
+            this.dtHasta.Name = "dtHasta";
+            this.dtHasta.Size = new System.Drawing.Size(89, 20);
+            this.dtHasta.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(402, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Desde";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(544, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Hasta";
+            // 
+            // btnFiltrar
+            // 
+            this.btnFiltrar.Location = new System.Drawing.Point(693, 103);
+            this.btnFiltrar.Name = "btnFiltrar";
+            this.btnFiltrar.Size = new System.Drawing.Size(75, 23);
+            this.btnFiltrar.TabIndex = 19;
+            this.btnFiltrar.Text = "Filtrar";
+            this.btnFiltrar.UseVisualStyleBackColor = true;
+            this.btnFiltrar.Click += new System.EventHandler(this.btnFiltrar_Click);
+            // 
             // ConsultarVuelosRealizados
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1143, 410);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1143, 544);
+            this.Controls.Add(this.btnFiltrar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtHasta);
+            this.Controls.Add(this.dtDesde);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnVuelos);
             this.Controls.Add(this.dtgVuelos);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "ConsultarVuelosRealizados";
-            this.Text = "ConsultarVuelosRealizados";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vuelos Realizados";
+            this.Load += new System.EventHandler(this.ConsultarVuelosRealizados_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dtgVuelos)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,5 +144,11 @@
 
         private System.Windows.Forms.DataGridView dtgVuelos;
         private System.Windows.Forms.Button btnVuelos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtDesde;
+        private System.Windows.Forms.DateTimePicker dtHasta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnFiltrar;
     }
 }
--- Aeoronautica4/Vistas/Consultor/ConsultarVuelosRealizados.cs
+++ Aeoronautica4/Vistas/Consultor/ConsultarVuelosRealizados.cs
@@ -65,12 +65,77 @@ namespace Aeronautica.Vistas.Consultor
                 dtgVuelos.Columns["TIPO_AERONAVE"].HeaderText = "TIPO DE AERONAVE";
                 dtgVuelos.Columns["MODELO"].Width = 158;
                 dtgVuelos.Columns["MODELO"].HeaderText = "MODELO";
+
+                dtHasta.Enabled = true;
+                dtDesde.Enabled = true;
+                btnFiltrar.Enabled = true;
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
 
             }
+        }
+
+        private void btnFiltrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OracleConnection conn = new OracleConnection((consultas.Variables.ConString));
+                conn.Open();
+                OracleCommand cmd = new OracleCommand(""+(consultas.Variables.BtnFiltrarVuelos)+" '"+dtDesde.Text+"' "+(consultas.Variables.BtnFiltrarVuelos2)+" '"+dtHasta.Text+"'", conn);
+                cmd.CommandType = CommandType.Text;
+                DataSet ds = new DataSet();
+                OracleDataAdapter da = new OracleDataAdapter();
+                da.SelectCommand = cmd;
+                da.Fill(ds);
+                dtgVuelos.DataSource = ds.Tables[0];
+                dtgVuelos.AllowUserToResizeColumns = true;
+                dtgVuelos.AllowUserToResizeRows = true;
+                dtgVuelos.MultiSelect = true;
+                dtgVuelos.Columns["ID_PLAN_VUELO_REAL"].Width = 158;
+                dtgVuelos.Columns["ID_PLAN_VUELO_REAL"].HeaderText = "ID DE VUELO";
+                dtgVuelos.Columns["FECHA_ORIGEN_REAL"].Width = 158;
+                dtgVuelos.Columns["FECHA_ORIGEN_REAL"].HeaderText = "FECHA DE ORIGEN";
+                dtgVuelos.Columns["FECHA_DESTINO_REAL"].Width = 158;
+                dtgVuelos.Columns["FECHA_DESTINO_REAL"].HeaderText = "FECHA DE DESTINO";
+                dtgVuelos.Columns["RUT_PILOTO"].Width = 158;
+                dtgVuelos.Columns["RUT_PILOTO"].HeaderText = "RUT PILOTO";
+                dtgVuelos.Columns["LICENCIA_PILOTO"].Width = 158;
+                dtgVuelos.Columns["LICENCIA_PILOTO"].HeaderText = "LICENCIA PILOTO";
+                dtgVuelos.Columns["RUT_COPILOTO"].Width = 158;
+                dtgVuelos.Columns["RUT_COPILOTO"].HeaderText = "RUT COPILOTO";
+                dtgVuelos.Columns["LICENCIA_COPILOTO"].Width = 158;
+                dtgVuelos.Columns["LICENCIA_COPILOTO"].HeaderText = "LICENCIA COPILOTO";
+                dtgVuelos.Columns["RUTA"].Width = 158;
+                dtgVuelos.Columns["RUTA"].HeaderText = "RUTA";
+                dtgVuelos.Columns["ORIGEN"].Width = 158;
+                dtgVuelos.Columns["ORIGEN"].HeaderText = "ORIGEN";
+                dtgVuelos.Columns["DESTINO"].Width = 158;
+                dtgVuelos.Columns["DESTINO"].HeaderText = "DESTINO";
+                dtgVuelos.Columns["CONDICION"].Width = 158;
+                dtgVuelos.Columns["CONDICION"].HeaderText = "CONDICION";
+                dtgVuelos.Columns["MISION"].Width = 158;
+                dtgVuelos.Columns["MISION"].HeaderText = "MISION";
+                dtgVuelos.Columns["MATRICULA"].Width = 158;
+                dtgVuelos.Columns["MATRICULA"].HeaderText = "MATRICULA";
+                dtgVuelos.Columns["TIPO_AERONAVE"].Width = 158;
+                dtgVuelos.Columns["TIPO_AERONAVE"].HeaderText = "TIPO DE AERONAVE";
+                dtgVuelos.Columns["MODELO"].Width = 158;
+                dtgVuelos.Columns["MODELO"].HeaderText = "MODELO";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void ConsultarVuelosRealizados_Load(object sender, EventArgs e)
+        {
+            dtHasta.Enabled = false;
+            dtDesde.Enabled = false;
+            btnFiltrar.Enabled = false;
         }
     }
 }
